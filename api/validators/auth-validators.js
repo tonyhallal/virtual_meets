213bcb8849722af15f49a92e9549a49bd7045d2f
Tony Hallal <tonihallal@gmail.com>
2023-12-22 00:25:08 +0200

    added chat functionality and migrated to nest js

--- api/validators/auth-validators.js
+++
@@ -1,6 +0,0 @@
-import {check} from "express-validator";
-
-export const authAndUserValidator = [
-    check('user_username').notEmpty().withMessage('username is missing'),
-    check('user_password').notEmpty().withMessage('password is missing')
-]
